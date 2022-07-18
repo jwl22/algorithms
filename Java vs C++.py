8b7adc8857daee888acbeb30e3f3a13a810cda70
jwl22 <jw.lee@yonsei.ac.kr>
2022-07-18 19:55:43 +0900

    No.3613

--- Java vs C++.py
+++ Java vs C++.py
@@ -1,61 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-
+variableName = input()
+variableName = variableName.replace("\n", "")
 arr = []
-for _ in range(0, N):
-    arr.append(list(map(int, input().split())))
 
-for i in range(0, N):
-    for j in range(0, M):
-        if arr[i][j] == 4:
-            start_i = i
-            start_j = j
-        elif arr[i][j] == 3:
-            if i-1 >= 0:
-                arr[i-1][j] = 1
-                if j-1 >= 0:
-                    arr[i-1][j-1] = 1
-                elif j+1 < M:
-                    arr[i-1][j+1] = 1
-            if j-1 >= 0:
-                arr[i][j-1] = 1
-            elif j+1 < M:
-                arr[i][j+1] = 1
-            if i+1 < N:
-                arr[i+1][j] = 1
-                if j-1 >= 0:
-                    arr[i+1][j-1] = 1
-                elif j+1 < M:
-                    arr[i+1][j+1] = 1
-        elif arr[i][j] == 2:
-            finish_i = i
-            finish_j = j
-count = 0
-head = 0, 1, 2, 3  # 0:up 1:down 2:left 3:right
+isJava = -1  # 0:C++ 1:JAVA
 
-if finish_i - start_i > 0:
-    head = 1
-elif finish_i - start_i < 0:
-    head = 0
-else:
-    if start_j - finish_j > 0:
-        head = 3
-    else:
-        head = 2
-while start_i != finish_i and start_j != finish_j:
-    if head == 0:
-        if arr[start_i-1][start_j] == 0:
-            arr[start_i][start_j]
-            start_i = start_i-1
-            count += 1
-        elif arr[start_i][start_j-1] == 0:
-            start_j = start_j-1
-            count += 1
-        elif arr[start_i][start_j+1] == 0:
-            start_j = start_j+1
-            count += 1
-        elif arr[start_i+1][start_]
+for i in variableName:
+    arr.append(i)
 
-print(count)
+result = ""
+isNext = 0
+for i in arr:
+    if i.isupper() == 1:
+        if isJava == 0 or result == "":
+            result = "Error!"
+            break
+        isJava = 1
+        result += '_' + i.lower()
+        continue
+    elif isNext == 1:
+        if i == '_':
+            result = "Error!"
+            break
+        result += i.upper()
+        isNext = 0
+        continue
+    elif i == "_":
+        if isJava == 1 or result == "":
+            result = "Error!"
+            break
+        isJava = 0
+        isNext = 1
+        continue
+
+    result += i
+
+if result == "" or isNext == 1:
+    result = "Error!"
+
+print(result)
